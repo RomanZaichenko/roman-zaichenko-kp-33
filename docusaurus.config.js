57439d67138fbb78323e9cc42a391295e370e0d7
Roman Zaichenko <romanzaichenko26@gmail.com>
2024-05-15 13:22:49 +0300

    Styles changed.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,10 +66,10 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Done.',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Not done',
+          src: 'img/done.svg',
         },
         items: [
           {
